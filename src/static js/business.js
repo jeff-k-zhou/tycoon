0bcb15f475f9fb1fb18c7ca152bd97303009cf4c
jeffzhou08 <86799981+jeffzhou08@users.noreply.github.com>
2022-04-17 20:07:36 -0500

    added files

---
+++ src/static js/business.js
@@ -0,0 +1,9 @@
+const business = [
+    {
+        name: "Fast Food",
+        starting_income: 1,
+        icon: "https://firebasestorage.googleapis.com/v0/b/tycoon-e1.appspot.com/o/burger.png?alt=media&token=24acc374-2ff1-440b-9bd0-b79006d0f178",
+    }
+]
+
+export {business}
