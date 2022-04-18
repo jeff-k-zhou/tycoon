bc9fec161816cf782196c107afdfb1d4886bc162
jeffzhou08 <86799981+jeffzhou08@users.noreply.github.com>
2022-04-18 10:27:59 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,27 @@ function App() {
   if (money === null) {
     setMoney(0)
   }
+  var i = 0
+  const move = (speed, income, id) => {
+    if (i == 0) {
+      i = 1;
+      var elem = document.getElementById(id);
+      var width = 1;
+      var id = setInterval(frame, speed);
+      function frame() {
+        if (width >= 100) {
+          clearInterval(id);
+          i = 0;
+          setMoney(prevMoney => parseInt(prevMoney) + income)
+          elem.style.width = 0 + "%"
+          localStorage.setItem("money", parseInt(money) + income)
+        } else {
+          width++;
+          elem.style.width = width + "%";
+        }
+      }
+    }
+  }
   return (
     <div class="container">
       <h1 class="title">Restaurant Tycoon</h1>
@@ -16,14 +37,35 @@ function App() {
         </div>
         <div class="main">
           <div class="tycoons">
+            <h1 class="header">Businesses</h1>
             {business.map((index) => {
               return (
                 <div class="industry">
-                  <div class = "top-div">
+                  <div class="top-div">
                     <img src={index.icon} class="ind-img" />
-                    <h1 class="ind-header">{index.name} - ${index.starting_income}/click</h1>
+                    <h1 class="ind-header">{index.name} - $
+                    {index.upgrade_status[0] === "true" && (index.upgrade_$[0])}
+                    {index.upgrade_status[1] === "true" && (index.upgrade_$[1])}
+                    {index.upgrade_status[2] === "true" && (index.upgrade_$[2])}
+                    {index.upgrade_status[3] === "true" && (index.upgrade_$[3])}
+                    {index.upgrade_status[4] === "true" && (index.upgrade_$[4])}
+                    {!index.upgrade_status[0] && !index.upgrade_status[1] && !index.upgrade_status[2] && !index.upgrade_status[3] && !index.upgrade_status[4] && (index.starting_income)}
+                      /click</h1>
                   </div>
-                  <div class = "bottom-div"></div>
+                  <div class="bottom-div">
+                    <button class="click" onClick={() => {
+                      for (var j = 0; j <= index.upgrade_status.length; j++) {
+                        if (index.upgrade_status[j] === "true") {
+                          move(index.upgrade_speed[j], index.upgrade_$[j], index.name)
+                        } else {
+                          move(index.init_speed, index.starting_income, index.name)
+                        }
+                      }
+                    }}>CLICK</button>
+                    <div class="progress">
+                      <div class="bar" id={index.name}></div>
+                    </div>
+                  </div>
                 </div>
               )
             })}
--- src/static js/business.js
+++ src/static js/business.js
@@ -3,7 +3,11 @@ const business = [
         name: "Fast Food",
         starting_income: 1,
         icon: "https://firebasestorage.googleapis.com/v0/b/tycoon-e1.appspot.com/o/burger.png?alt=media&token=24acc374-2ff1-440b-9bd0-b79006d0f178",
-    }
+        init_speed: 10,
+        upgrade_status: [localStorage.getItem("ff_upgrade1"), localStorage.getItem("ff_upgrade2"), localStorage.getItem("ff_upgrade3"), localStorage.getItem("ff_upgrade4"), localStorage.getItem("ff_upgrade5")],
+        upgrade_speed: [10,10,5,5,1],
+        upgrade_$: [2,3,3,5,10]
+    },
 ]
 
-export {business}
+export { business }
